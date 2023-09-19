0eb8407ed27338e59b420efc3153d725b0d2b97c
Melissa Sieu <melissasieu@wifi-172-23-64-232.uoa-wifi.auckland.ac.nz>
2023-09-19 23:36:47 +1200

    Feature: Updated the graphics for the title page and implemented a press key to start banner that runs across the bottom - code in the initialise of the title controller.

--- src/main/java/nz/ac/auckland/se206/controllers/TitleController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/TitleController.java
@@ -1,25 +1,66 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 
 public class TitleController {
 
   @FXML private Pane pane;
+  @FXML private Label movingTextLabel;
+  @FXML private Label movingTextCopyLabel;
 
-  public void initialize() {}
+  public void initialize() {
+    pane.layoutBoundsProperty()
+        .addListener(
+            (observable, oldValue, newValue) -> {
+              double windowWidth =
+                  newValue.getWidth(); // Update window width when layout bounds change
+
+              // Create a Timeline animation to move the text continuously
+              Timeline timelineMain =
+                  new Timeline(
+                      new KeyFrame(
+                          Duration.ZERO, new KeyValue(movingTextLabel.translateXProperty(), 0)),
+                      new KeyFrame(
+                          Duration.seconds(4),
+                          new KeyValue(movingTextLabel.translateXProperty(), windowWidth)));
+
+              timelineMain.setCycleCount(Timeline.INDEFINITE); // Repeat indefinitely
+
+              // Create a second KeyFrame to reset the text position when it goes beyond the
+              // window's width
+              Timeline timelineCopy =
+                  new Timeline(
+                      new KeyFrame(
+                          Duration.ZERO, new KeyValue(movingTextCopyLabel.translateXProperty(), 0)),
+                      new KeyFrame(
+                          Duration.seconds(4),
+                          new KeyValue(movingTextCopyLabel.translateXProperty(), windowWidth)));
+
+              timelineCopy.setCycleCount(Timeline.INDEFINITE); // Repeat indefinitely
+
+              // Start the animation
+              timelineMain.playFromStart(); // Ensure the animation starts from the beginning
+              timelineCopy.playFromStart(); // Ensure the animation starts from the beginning
+            });
+  }
 
   @FXML
-  public void onMouseClick(MouseEvent event) {
+  public void onClickMouse(MouseEvent event) {
     switchToMenu();
   }
 
   @FXML
-  public void onKeyPress(KeyEvent event) {
+  public void onPressKey(KeyEvent event) {
     switchToMenu();
   }
 
