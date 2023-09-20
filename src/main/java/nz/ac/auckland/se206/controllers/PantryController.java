ed3dc9363a6dd9188946ffd99a2c0c5cf15e9018
twoandtwelve <jackykim573@gmail.com>
2023-09-20 23:40:04 +1200

    Made the back button scale up when hovered over

--- src/main/java/nz/ac/auckland/se206/controllers/PantryController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/PantryController.java
@@ -1,23 +1,51 @@
 package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.Hover;
 import nz.ac.auckland.se206.SceneManager.AppUi;
 
 public class PantryController {
 
+  @FXML private Pane pane;
   @FXML private ImageView back;
 
-  public void initialize() {}
+  public void initialize() {
+    pane.requestFocus();
+  }
 
   @FXML
   public void clickBack(MouseEvent event) {
     switchToRoom();
   }
 
+  @FXML
+  public void onKeyPressed(KeyEvent event) {
+    if (event.getCode() == KeyCode.ESCAPE) {
+      switchToRoom();
+    }
+  }
+
   private void switchToRoom() {
     App.setUi(AppUi.MAIN_ROOM);
+  }
+
+  @FXML
+  public void onHoverInteractable(MouseEvent event) {
+
+    ImageView image = (ImageView) (Node) event.getTarget();
+    Hover.scaleUp(image);
+  }
+
+  @FXML
+  public void onLeaveInteractable(MouseEvent event) {
+    ImageView image = (ImageView) (Node) event.getTarget();
+    Hover.scaleDown(image);
   }
 }
