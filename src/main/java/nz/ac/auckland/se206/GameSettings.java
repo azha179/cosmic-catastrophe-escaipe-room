5e9d75bd5f7ef299475e1bc21318b9a1df5789bf
Melissa Sieu <melissasieu@wifi-172-23-64-232.uoa-wifi.auckland.ac.nz>
2023-09-20 20:11:46 +1200

    Feature: Created the game state for the items for the hub under GameDifficulty

--- src/main/java/nz/ac/auckland/se206/GameSettings.java
+++ src/main/java/nz/ac/auckland/se206/GameSettings.java
@@ -17,4 +17,13 @@ public class GameSettings {
   public static GameDifficulty difficulty = null;
 
   public static TimeLimit timeLimit = null;
+
+  // items for the hub once true will go into the hub
+  // if they are used, they will become false and grey out in the hub
+  // logic for the grey out will be the game progress state and item false
+  public static boolean torchFound = false;
+
+  public static boolean note1Found = false;
+
+  public static boolean note2Found = false;
 }
