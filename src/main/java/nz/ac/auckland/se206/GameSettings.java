a4d7380205264d94c87ae108e9a8847e0eaf5d30
Melissa Sieu <melissasieu@wifi-172-23-64-232.uoa-wifi.auckland.ac.nz>
2023-09-20 20:31:20 +1200

    Created the HUD for the items and created the gamestates for puzzles and footprints.

--- src/main/java/nz/ac/auckland/se206/GameSettings.java
+++ src/main/java/nz/ac/auckland/se206/GameSettings.java
@@ -19,11 +19,18 @@ public class GameSettings {
   public static TimeLimit timeLimit = null;
 
   // items for the hub once true will go into the hub
-  // if they are used, they will become false and grey out in the hub
-  // logic for the grey out will be the game progress state and item false
+  // logic for the grey out will be the game progress state and item true
   public static boolean torchFound = false;
+
+  public static boolean footprintsFound = false;
 
   public static boolean note1Found = false;
 
   public static boolean note2Found = false;
+
+  public static boolean puzzle1 = false;
+
+  public static boolean puzzle2 = false;
+
+  public static boolean puzzle3 = false;
 }
