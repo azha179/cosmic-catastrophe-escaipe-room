31bca1005041da3a73182c68b73f784d6844d56a
twoandtwelve <jackykim573@gmail.com>
2023-09-20 19:49:16 +1200

    Added buttons to memory game scene, each button activates pressbutton and releasebutton

--- src/main/java/nz/ac/auckland/se206/controllers/MemoryGameController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MemoryGameController.java
@@ -9,6 +9,22 @@ import nz.ac.auckland.se206.SceneManager.AppUi;
 public class MemoryGameController {
 
   @FXML private ImageView back;
+  @FXML private ImageView button0;
+  @FXML private ImageView button1;
+  @FXML private ImageView button2;
+  @FXML private ImageView button3;
+  @FXML private ImageView button4;
+  @FXML private ImageView button5;
+  @FXML private ImageView button6;
+  @FXML private ImageView button7;
+  @FXML private ImageView button8;
+  @FXML private ImageView button9;
+  @FXML private ImageView button10;
+  @FXML private ImageView button11;
+  @FXML private ImageView button12;
+  @FXML private ImageView button13;
+  @FXML private ImageView button14;
+  @FXML private ImageView button15;
 
   public void initialize() {}
 
@@ -19,5 +35,17 @@ public class MemoryGameController {
 
   private void switchToRocket() {
     App.setUi(AppUi.ROCKET_INTERIOR);
+  }
+
+  @FXML
+  private void pressButton(MouseEvent event) {
+    System.out.println("press button");
+    ImageView image = (ImageView) event.getTarget();
+    System.out.println(image);
+  }
+
+  @FXML
+  private void releaseButton(MouseEvent event) {
+    System.out.println("release button");
   }
 }
