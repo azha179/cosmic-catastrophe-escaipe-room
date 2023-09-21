858e3149d142b53b12371a38dd95bc0846b3b078
Melissa Sieu <melissasieu@wifi-172-23-64-232.uoa-wifi.auckland.ac.nz>
2023-09-21 21:30:06 +1200

    added the files for the commit

---
+++ src/main/java/nz/ac/auckland/se206/controllers/SettingsController.java
@@ -0,0 +1,17 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleButton;
+
+public class SettingsController {
+  @FXML private Button backButton;
+  @FXML private Button applyButton;
+  @FXML private Slider volumeSlider;
+  @FXML private ToggleButton ttsToggleButton;
+  @FXML private ChoiceBox resolutionButton;
+
+  public void initialize() {}
+}
