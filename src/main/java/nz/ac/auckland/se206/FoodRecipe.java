560ec2afd8a1c216237005c2a7274f18cbe41cb9
twoandtwelve <jackykim573@gmail.com>
2023-09-21 20:45:57 +1200

    Added storing each ingredient to collections and assigned user data to each

---
+++ src/main/java/nz/ac/auckland/se206/FoodRecipe.java
@@ -0,0 +1,12 @@
+package nz.ac.auckland.se206;
+
+import java.util.ArrayList;
+import javafx.scene.image.ImageView;
+
+public class FoodRecipe {
+
+  public static ArrayList<ImageView> prefixIngredient = new ArrayList<ImageView>();
+  public static ArrayList<ImageView> baseIngredient = new ArrayList<ImageView>();
+  public static ArrayList<ImageView> desiredRecipe = new ArrayList<ImageView>();
+  public static ArrayList<ImageView> playerRecipe = new ArrayList<ImageView>();
+}
