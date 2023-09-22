6718387f9d2a79db76da3ba888ea4a9fb57acf00
Melissa Sieu <melissasieu@wifi-172-23-64-232.uoa-wifi.auckland.ac.nz>
2023-09-22 19:59:48 +1200

    Feature: cat will meow in initial wake up.

--- src/main/java/nz/ac/auckland/se206/controllers/MainRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MainRoomController.java
@@ -27,6 +27,7 @@ import nz.ac.auckland.se206.SceneManager.AppUi;
 import nz.ac.auckland.se206.gpt.ChatMessage;
 import nz.ac.auckland.se206.gpt.GptPromptEngineering;
 import nz.ac.auckland.se206.gpt.openai.ChatCompletionRequest;
+import nz.ac.auckland.se206.speech.TextToSpeech;
 
 /** Controller class for the room view. */
 public class MainRoomController {
@@ -199,6 +200,24 @@ public class MainRoomController {
                       });
                   // Enable cat
                   catImageSleep.setDisable(false);
+                });
+
+            Platform.runLater(
+                () -> {
+
+                  // Text to speech task
+                  Task<Void> textToSpeechTask =
+                      new Task<Void>() {
+                        @Override
+                        protected Void call() throws Exception {
+                          TextToSpeech textToSpeech = new TextToSpeech();
+                          textToSpeech.speak("Meow");
+                          return null;
+                        }
+                      };
+
+                  Thread textToSpeechThread = new Thread(textToSpeechTask);
+                  textToSpeechThread.start();
                 });
 
             return null;
